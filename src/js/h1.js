0eea64f56c3f401d35b9756ab6c70b957fa7b491
Vanja7skr <141807701+Vanja7skr@users.noreply.github.com>
2026-03-30 13:37:37 -0500

    h1 and fonts

---
+++ src/js/h1.js
@@ -0,0 +1,14 @@
+const h1 = document.createElement('h1');
+
+h1.textContent = 'Популярні інетрактивні ігри';
+h1.style.margin = '0 auto';
+
+h1.style.display = 'block';
+h1.style.width = 'fit-content';
+h1.style.fontFamily = 'var(--font-family)';
+
+h1.style.fontSize = '24px';
+h1.style.color = '#000';
+
+console.log(h1);
+document.body.appendChild(h1);
