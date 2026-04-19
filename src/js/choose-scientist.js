e2a8758a01236db44f053d45d17a289e9bd8a189
TimurG1 <timur.ganziy@gmail.com>
2026-04-19 17:03:30 +0200

    add logik
    Докидую секцію наша команда і продовжую  роботу над "Обери вченого/их"

--- src/js/choose-scientist.js
+++ src/js/choose-scientist.js
@@ -10,12 +10,29 @@ function createEl(tag, className, text, numberOftags = 1) {
   return numberOftags === 1 ? elements[0] : elements;
 }
 
-const scientisTitle = createEl('h2', 'scientis-title', 'Обери вченого/их');
-const scientisList = createEl('ul', 'scientis-list');
-const scientisImg = createEl('img', 'scientis-item', undefined, 16);
-const scientisBtnList = createEl('ul', 'scientis-btn-list');
-const scientisBtn = createEl('button', 'scientis-btn-item', undefined, 9);
+const scientistTitle = createEl('h2', 'scientist-title', 'Обери вченого/их');
+const scientistList = createEl('ul', 'scientist-list');
+const scientistImg = createEl('img', 'scientist-item', undefined, 12);
+const scientistBtnList = createEl('ul', 'scientist-btn-list');
+const scientistBtn = createEl('button', 'scientist-btn-item', undefined, 9);
+chooseScientistRoot.append(scientistTitle, scientistList, scientistBtnList);
+scientistList.append(...scientistImg);
+scientistBtnList.append(...scientistBtn);
 
-chooseScientistRoot.append(scientisTitle, scientisList, scientisBtnList);
-scientisList.append(...scientisImg);
-scientisBtnList.append(...scientisBtn);
+const textBtn = [
+  'Які вчені народилися в 19 ст.',
+  'Знайти рік народження Albert Einshtein',
+  'Відсортувати вчених за алфавітом',
+  'Знайти вчених, прізвища яких починаються на на літеру “С” ',
+  'Відсортувати вчених за кількістю прожитих років',
+  'Видалити всіх вчених, ім’я яких починається на “А”',
+  'Знайти вченого, який народився найпізніше',
+  'Знайти вченого, який прожив найдовше і вченого, який прожив найменше',
+  'Знайти вчених, в яких співпадають перші літери імені і прізвища',
+];
+function addText(arrayEl, arrayText) {
+  for (let i = 0; i < arrayEl.length; i++) {
+    arrayEl[i].textContent = arrayText[i];
+  }
+}
+addText(scientistBtn, textBtn);
--- src/js/our-team.js
+++ src/js/our-team.js
@@ -15,7 +15,7 @@ function createEl(tag, className, text, numberOftags = 1) {
 const teamTitle = createEl('h2', 'team-title', 'Наша команда');
 console.log(teamTitle);
 const teamImg = createEl('img', 'team-img');
-teamImg.src = './img/photo5395423997429749346y.jpg'
+teamImg.src = './img/andrian.jpg';
 teamImg.alt = "фото Ім'я";
 const teamName = createEl('h3', 'team-name', 'Ім’я студента');
 const teamInfo = createEl(
@@ -31,17 +31,18 @@ const arrowRightSvg = createEl('svg', 'team-svg--r');
 const arrowRightIcon = createEl('use', 'team-icon--r');
 
 // arrowRightSvg.style.width = "150px";
-arrowLeftIcon.setAttribute("href", './icons/symboldefs.svg#icon-rock')  ;
-console.log(arrowLeftIcon)
-arrowRightIcon.href = ""
-arrowLeftBtn.append(arrowLeftSvg)
+arrowLeftIcon.setAttribute('href', './icons/symboldefs.svg#icon-rock');
+console.log(arrowLeftIcon);
+arrowRightIcon.href = '';
+arrowLeftBtn.append(arrowLeftSvg);
 arrowLeftSvg.append(arrowLeftIcon);
 arrowRightBtn.append(arrowRightSvg);
 arrowRightSvg.append(arrowRightIcon);
 
-
 const lineList = createEl('ul', 'line-list');
-const lineItem = createEl('li', 'line-item', undefined, 7);
+const lineItem = createEl('li', 'line-item', undefined, 5);
+lineItem[0].classList.add('hovered');
+console.log(lineItem)
 
 ourTeamSection.append(
   teamTitle,
@@ -53,3 +54,61 @@ ourTeamSection.append(
   arrowRightBtn
 );
 lineList.append(...lineItem);
+
+let currentIndex = 0;
+let lastIndex = 0
+const imgArr = [
+  './img/andrian.jpg',
+  './img/ivanS.jpg',
+  './img/timur.jpg',
+  './img/ivanK.jpg',
+  './img/olena.jpg',
+];
+const nameArr = [
+  'Андріа Костик',
+  'Іван Скрипка',
+  'Тимур Ганзій',
+  'Іван Карпенко',
+  'Олена Наумчик',
+];
+const infoArr = [
+  'Team lead нашої команди',
+  'Я скрам-майстер і розробляв секцію "Наша команда" і "Обери вченого/их"',
+  'Я розробляв секції "Наша команда" і "Обери вченого/их"',
+  '',
+  '',
+];
+
+function changeInfo() {
+  teamImg.src = imgArr[currentIndex];
+  teamName.textContent = nameArr[currentIndex];
+  teamInfo.textContent = infoArr[currentIndex];
+  lineItem[currentIndex].classList.add("hovered");
+  lineItem[lastIndex].classList.remove("hovered");
+
+}
+arrowRightBtn.addEventListener('click', scrollRight);
+function scrollRight() {
+  if (currentIndex < imgArr.length - 1) {
+    lastIndex = currentIndex
+    currentIndex++;
+    changeInfo();
+  } else {
+    lastIndex = currentIndex;
+    currentIndex = 0;
+    changeInfo();
+  }
+}
+arrowLeftBtn.addEventListener('click', scrollLeft);
+function scrollLeft() {
+  if (currentIndex > 0) {
+    lastIndex = currentIndex
+    currentIndex--;
+    changeInfo();
+  } else {
+    lastIndex = currentIndex;
+    currentIndex = 4;
+    changeInfo();
+  }
+}
+
